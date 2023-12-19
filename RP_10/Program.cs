83ce32d4727a7f08676f68378f22371b8af80aa5
GGevA <GGevA@DESKTOP-OIFFF4J>
2023-12-20 01:51:55 +0300

    Добавьте файлы проекта.

---
+++ RP_10/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Drawing;
+
+namespace PR_10
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                var userInSystem = Auto();
+                Console.Clear();
+                if (userInSystem.role == "Administrator")
+                {
+                    var administrator = new Administrator();
+                    administrator.ADMenu(userInSystem);
+                }
+            }
+        }
+        static User Auto()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Введите логин");
+                string login = Console.ReadLine();
+                Console.WriteLine("Введите пароль");
+                string password = string.Empty;
+                ConsoleKeyInfo info;
+                while (true)
+                {
+                    info = Console.ReadKey(true);
+                    if (info.Key == ConsoleKey.Enter)
+                        break;
+                    password += info.KeyChar;
+                    Console.Write('*');
+                }
+                Console.WriteLine();
+                foreach (var user in SerealizationNDeserialization.GetData())
+                {
+                    if (user.login == login)
+                        if (user.password == password)
+                        {
+                            var loggedUser = new User(user.id, login, password, user.role);
+                            return loggedUser;
+                        }
+                }
+                Console.WriteLine("Логин или пароль неверный");
+            }
+        }
+    }
+}
---
+++ RP_10/SerealizationNDeserialization.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Linq;
+
+namespace PR_10
+{
+    internal static class SerealizationNDeserialization
+    {
+        static string path = $@"{Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory)}\save";
+        public static void AdminSerialization(User userData)
+        {
+                var usersList = GetData();
+                usersList.Add(userData);
+                string json = JsonConvert.SerializeObject(usersList);
+                File.WriteAllText($@"{path}\Users.json", json);   
+        }
+        public static List<User> GetData()
+        {
+            if (!File.Exists($@"{path}\Users.json"))
+            {
+                var newList = new List<User>();
+                var admin = new User(0, "admin", "admin", "Administrator");
+                Directory.CreateDirectory(path);
+                newList.Add(admin);
+                string json = JsonConvert.SerializeObject(newList);
+                File.Create($@"{path}\Users.json").Close();
+                File.WriteAllText($@"{path}\Users.json", json);
+            }
+            string data = File.ReadAllText($@"{path}\Users.json");
+            List<User> usersDataList = JsonConvert.DeserializeObject<List<User>>(data);
+            var sortedUsersDataList = from user in usersDataList
+                                      orderby user.id
+                                      select user;
+            usersDataList = sortedUsersDataList.ToList();
+            return usersDataList;
+        }
+    }
+}
